956d1415ed0edff051cd836bfb993fb5d31df409
raaji-ch <rajeswari.tib@gmail.com>
2017-01-24 19:32:37 -0600

    Initial commit

---
+++ src/main/java/com/example/myapis.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class myapis {
+
+    @RequestMapping("/greeting")
+    @ResponseBody
+    String home() {
+    	System.out.println("Received request for this api");
+        return "Hello World!";
+    }
+    
+    @RequestMapping(value="/greet/{name}", method = RequestMethod.GET)
+    @ResponseBody
+    String greet(@PathVariable("name") String name) {
+        return "Hi, "+name;
+    }
+}
